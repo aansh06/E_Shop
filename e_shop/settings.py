b7347df4eb23318e86e2013a48f3822cd27ea222
aansh06 <95288420+aansh06@users.noreply.github.com>
2024-05-03 17:48:40 +0000

    product_model, static_path_setup

--- e_shop/settings.py
+++ e_shop/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,6 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+# Set the MEDIA_ROOT to the 'media' directory within your project's base directory
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Define the URL path for serving media files
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
