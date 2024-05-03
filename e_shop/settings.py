ed668388d46a0a5062827ae449d82e8bc67346c1
aansh06 <95288420+aansh06@users.noreply.github.com>
2024-05-03 15:38:53 +0000

    added product model,superuser

--- e_shop/settings.py
+++ e_shop/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "shop.apps.ShopConfig",
+
+
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -38,7 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    "shop",
+   
 ]
 
 MIDDLEWARE = [
