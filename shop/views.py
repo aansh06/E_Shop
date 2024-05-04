e19cb66548d68bc3a6d3fb70cd5c157b1d0a62ed
aansh06 <95288420+aansh06@users.noreply.github.com>
2024-05-04 18:37:19 +0000

    added_item_carousel

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Product
+from math import ceil
+
+
+
+
 # Create your views here.
 def index(request):
-    return render(request,"shop/index.html")
+    products = Product.objects.all()
+    slides = [products[i:i+4] for i in range(0, len(products), 4)]  # Group products into slides
+
+    # Check if the last slide has fewer than 4 products
+    if len(products) % 4 != 0:
+        remaining_products = len(products) % 4
+        last_slide = products[-remaining_products:]
+        slides.append(last_slide)
+    
+    return render(request, 'shop/index.html', {'slides': slides})    
 
 def about(request):
-    return HttpResponse("Know about us .....")
+    return render(request,"shop/about.html")
 
 def contact(request):
     return HttpResponse("contact us .....")
